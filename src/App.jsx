c9db6ba54fc639ac9b2ea94faa1ca235c9702e92
carlosvillaar <carlosvillar004@gmail.com>
2025-01-14 18:12:41 +0100

    feat: import data and send to child

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./assets/components/Header";
 import Main from "./assets/components/Main";
 import Footer from "./assets/components/Footer";
-import comics from "../data/comics";
+import {headerLinks, footerShop, footerDC, footerSites} from "../data/comicsData";
 
 const App = () => {
+
+
+
   return (
     <>
-      <Header />
+      <Header headerLinks={headerLinks} />
       <Main />
       <Footer />
     </>
