728969c239ba9864bd0aba9cb51b70efc880d139
carlosvillaar <carlosvillar004@gmail.com>
2025-01-14 17:19:32 +0100

    feat: import data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Header from "./assets/components/Header"
-import Main from "./assets/components/Main"
-import Footer from "./assets/components/Footer"
+import Header from "./assets/components/Header";
+import Main from "./assets/components/Main";
+import Footer from "./assets/components/Footer";
+import comics from "../data/comics";
 
 const App = () => {
   return (
