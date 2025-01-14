a9bba08d25e831b887ef84764e56f89c1dace250
carlosvillaar <carlosvillar004@gmail.com>
2025-01-14 17:31:24 +0100

    feat: add header and footer objects

---
+++ data/comicsData.js
@@ -0,0 +1,40 @@
+const HeaderLinks = [
+  { id: 1, url: "#", name: "CHARACTERS"},
+  { id: 2, url: "#", name: "COMICS"},
+  { id: 3, url: "#", name: "MOVIES"},
+  { id: 4, url: "#", name: "TV"},
+  { id: 5, url: "#", name: "GAMES"},
+  { id: 6, url: "#", name: "COLLECTIBLES"},
+  { id: 7, url: "#", name: "VIDEOS"},
+  { id: 8, url: "#", name: "FANS"},
+  { id: 9, url: "#", name: "NEWS"},
+  { id: 10, url: "#", name: "SHOP"}
+ ];
+
+const footerShop = [
+  {id: 1, url: "#", text:"Shop DC"},
+  {id: 2, url: "#", text:"Shop DC Collectibles"}
+]
+
+const footerDC = [
+  { id: 1, url: "#", text: "Therms Of Use" },
+  { id: 2, url: "#", text: "Privacy policy (New)" },
+  { id: 3, url: "#", text: "Ad Choices" },
+  { id: 4, url: "#", text: "Advertising" },
+  { id: 5, url: "#", text: "Jobs" },
+  { id: 6, url: "#", text: "Subscriptions" },
+  { id: 7, url: "#", text: "Talent Workshops" },
+  { id: 8, url: "#", text: "CPSC Certificates" },
+  { id: 9, url: "#", text: "Ratings" },
+  { id: 10, url: "#", text: "Shop Help" },
+  { id: 11, url: "#", text: "Contact Us" }
+];
+
+const footerSites = [
+ { id: 1, url: "#", text: "DC" },
+ { id: 2, url: "#", text: "MAD Magazine" },
+ { id: 3, url: "#", text: "DC Kids" },
+ { id: 4, url: "#", text: "DC Universe" },
+ { id: 5, url: "#", text: "DC Power Visa" }
+];
+
