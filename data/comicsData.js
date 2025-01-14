1b0f96d4747dcbe7a597b28e08800d448083e7b1
carlosvillaar <carlosvillar004@gmail.com>
2025-01-14 18:31:45 +0100

    fix: update properties name

--- data/comicsData.js
+++ data/comicsData.js
@@ -24,30 +24,30 @@ const footerDcComics = [
 ]
 
 const footerShop = [
-  {id: 1, url: "#", text:"Shop DC"},
-  {id: 2, url: "#", text:"Shop DC Collectibles"}
+  {id: 1, url: "#", name:"Shop DC"},
+  {id: 2, url: "#", name:"Shop DC Collectibles"}
 ]
 
 const footerDC = [
-  { id: 1, url: "#", text: "Therms Of Use" },
-  { id: 2, url: "#", text: "Privacy policy (New)" },
-  { id: 3, url: "#", text: "Ad Choices" },
-  { id: 4, url: "#", text: "Advertising" },
-  { id: 5, url: "#", text: "Jobs" },
-  { id: 6, url: "#", text: "Subscriptions" },
-  { id: 7, url: "#", text: "Talent Workshops" },
-  { id: 8, url: "#", text: "CPSC Certificates" },
-  { id: 9, url: "#", text: "Ratings" },
-  { id: 10, url: "#", text: "Shop Help" },
-  { id: 11, url: "#", text: "Contact Us" }
+  { id: 1, url: "#", name: "Therms Of Use" },
+  { id: 2, url: "#", name: "Privacy policy (New)" },
+  { id: 3, url: "#", name: "Ad Choices" },
+  { id: 4, url: "#", name: "Advertising" },
+  { id: 5, url: "#", name: "Jobs" },
+  { id: 6, url: "#", name: "Subscriptions" },
+  { id: 7, url: "#", name: "Talent Workshops" },
+  { id: 8, url: "#", name: "CPSC Certificates" },
+  { id: 9, url: "#", name: "Ratings" },
+  { id: 10, url: "#", name: "Shop Help" },
+  { id: 11, url: "#", name: "Contact Us" }
 ];
 
 const footerSites = [
- { id: 1, url: "#", text: "DC" },
- { id: 2, url: "#", text: "MAD Magazine" },
- { id: 3, url: "#", text: "DC Kids" },
- { id: 4, url: "#", text: "DC Universe" },
- { id: 5, url: "#", text: "DC Power Visa" }
+ { id: 1, url: "#", name: "DC" },
+ { id: 2, url: "#", name: "MAD Magazine" },
+ { id: 3, url: "#", name: "DC Kids" },
+ { id: 4, url: "#", name: "DC Universe" },
+ { id: 5, url: "#", name: "DC Power Visa" }
 ];
 
 export {
