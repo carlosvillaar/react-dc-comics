9b26528a7abb3bb783777d826f687dee95235e4b
carlosvillaar <carlosvillar004@gmail.com>
2025-01-15 12:55:19 +0100

    feat: import data file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./assets/components/Header";
 import Main from "./assets/components/Main";
 import Footer from "./assets/components/Footer";
 import { headerLinks, footerDcComics, footerShop, footerDC, footerSites } from "../data/comicsData";
+import comics from "../data/comics";
 
 const App = () => {
 
@@ -10,7 +11,7 @@ const App = () => {
   return (
     <>
       <Header headerLinks={headerLinks} />
-      <Main />
+      <Main comics={comics} />
       <Footer footerMenus={{ footerDcComics, footerShop, footerDC, footerSites }} />
     </>
   )
